ecb20e189bc69ace205b7acc28920d68542ebcc5
juancarlos-fuentes-by <91288753+juancarlos-fuentes-by@users.noreply.github.com>
2022-12-12 08:34:19 -0600

    first commit

---
+++ src/main/java/com/example/microservicetrainingby/MicroserviceTrainingByApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicetrainingby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTrainingByApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceTrainingByApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/microservicetrainingby/MicroserviceTrainingByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicetrainingby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTrainingByApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
